f70dda078c21ecefb205e706b9dc6223b7034919
Spyros Vlachos <devspyrosv@users.noreply.github.com>
2018-11-12 22:40:58 +0200

    openvpn-run.sh: add options to prevent DNS leaks

--- openvpn/bin/openvpn-run.sh
+++ openvpn/bin/openvpn-run.sh
@@ -6,4 +6,7 @@ echo "OpenVPN config: ${OPENVPN_CONFIG}"
 exec openvpn --script-security 2 \
     --up "/etc/openvpn/bin/openvpn-up.sh" \
     --down "/etc/openvpn/bin/openvpn-down.sh" \
+    --down-pre \
+    --dhcp-option "DNSSEC allow-downgrade" \
+    --dhcp-option "DOMAIN-ROUTE ." \
     $OPENVPN_OPTS --config "$OPENVPN_CONFIG"
